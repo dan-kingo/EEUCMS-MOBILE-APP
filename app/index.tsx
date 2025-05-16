b61b089c50617b8eb744d0df88bce43fa0d0617c
Dan Kingo <danieldejen23@gmail.com>
2025-05-16 15:59:45 +0300

    inital commit

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl text-center font-bold">
+        Welcome to EEU Complaint System
+      </Text>
     </View>
   );
 }
